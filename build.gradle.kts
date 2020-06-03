9818486554b3500dc553b6250ad33ff26401ce5c
Jonas <48187105+jonaznas@users.noreply.github.com>
2020-06-03 23:35:13 +0200

    Remove unused tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,32 +7,3 @@ subprojects {
         }
     }
 }
-
-tasks {
-    /*"buildClient" {
-        dependsOn("cleanClient", "client:buildFromNode")
-        group = "application"
-        doLast {
-            // TODO: Move dist folder to build
-        }
-    }*/
-
-    /*"buildServer" {
-        dependsOn("cleanServer", "server:jar")
-        group = "application"
-    }
-
-    "cleanClient" {
-        typeOf<Delete>()
-        delete("$buildDir/client")
-    }
-
-    "cleanServer" {
-        typeOf<Delete>()
-        delete("$buildDir/server")
-    }
-
-    "bootRun" {
-        group = "other"
-    }*/
-}
