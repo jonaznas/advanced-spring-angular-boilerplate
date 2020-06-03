3f30d2067c464b8f04f8db16bad9aec97a8e2174
Jonas <48187105+jonaznas@users.noreply.github.com>
2020-06-03 15:39:04 +0200

    Remove unused encryption methods

--- server/src/main/kotlin/dev/jonaz/backend/component/auth/Encryption.kt
+++ server/src/main/kotlin/dev/jonaz/backend/component/auth/Encryption.kt
@@ -1,25 +1,9 @@
 package dev.jonaz.backend.component.auth
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import java.security.MessageDigest
 import java.security.SecureRandom
-
-fun String.md5(): String {
-    return hashString(this, "MD5")
-}
-
-fun String.sha256(): String {
-    return hashString(this, "SHA-256")
-}
 
 fun String.bcrypt(): String {
     val encoder = BCryptPasswordEncoder(10, SecureRandom())
     return encoder.encode(this)
-}
-
-private fun hashString(input: String, algorithm: String): String {
-    return MessageDigest
-            .getInstance(algorithm)
-            .digest(input.toByteArray())
-            .fold("", { str, it -> str + "%02x".format(it) })
 }
