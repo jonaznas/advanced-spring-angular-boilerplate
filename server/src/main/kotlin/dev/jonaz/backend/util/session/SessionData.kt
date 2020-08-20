359bc6078b8382a2dfda5c15c3c145e2f95c8c5d
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-08-20 17:22:02 +0200

    Remove socket.io

--- server/src/main/kotlin/dev/jonaz/backend/util/session/SessionData.kt
+++
@@ -1,9 +0,0 @@
-package dev.jonaz.backend.util.session
-
-import java.net.InetAddress
-
-data class SessionData(
-        val user: Int? = null,
-        val token: String? = null,
-        val remoteAddress: InetAddress? = null
-)
--- server/src/main/kotlin/dev/jonaz/backend/util/socket/SocketMapping.kt
+++
@@ -1,5 +0,0 @@
-package dev.jonaz.backend.util.socket
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
-annotation class SocketMapping(val path: String, val permission: SocketGuard)
